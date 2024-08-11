4b20a9d3ebaea269c87e5830db98a5747056c665
Amal221200 <amalmurikkoli2000@gmail.com>
2024-08-11 14:33:22 +0530

    responsive form

--- src/app/_components/Main.tsx
+++ src/app/_components/Main.tsx
@@ -2,15 +2,15 @@ import StakingForm from "./StakingForm"
 
 const Main = () => {
   return (
-    <main className="sm:h-[80dvh]">
-      <div className="flex h-full bg-white/20 p-3 bg-blend-overlay sm:bg-gradient sm:bg-contain sm:bg-left sm:bg-no-repeat">
+    <main className="h-[60dvh] sm:h-[80dvh]">
+      <div className="flex h-full bg-white/20 bg-blend-overlay sm:bg-gradient sm:bg-contain sm:bg-left sm:bg-no-repeat sm:p-3">
         <div className="hidden h-full flex-1 items-center justify-start sm:flex">
           <div className="">
             <h1 className="text-xl font-semibold md:text-3xl">Welcome to Staking App</h1>
             <p className="text-sm md:text-lg">Stake your BTC and contribute to the network collateral.</p>
           </div>
         </div>
-        <div className="flex h-full flex-1 flex-col items-center justify-center gap-y-3">
+        <div className="flex h-full flex-1 flex-col items-center justify-center gap-y-3 bg-gradient bg-cover bg-center bg-no-repeat p-3 bg-blend-overlay sm:bg-none sm:p-0">
         <div className="text-center sm:hidden">
             <h1 className="text-lg font-semibold">Welcome to Staking App</h1>
             <p className="text-sm">Stake your BTC and contribute to the network collateral.</p>
